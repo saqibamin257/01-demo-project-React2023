a52a7fc4007fe040f0583c53d3578cb48bdfe2fa
Saqib Amin <saqibamin257@gmail.com>
2023-07-07 16:01:27 +0500

    UseContext Hook-Pass Data Prent-Child&child-parent

--- src/index.js
+++ src/index.js
@@ -2190,33 +2190,105 @@
 
 //#region Exe-31 - UseContext Hook Basics
 
-import React,{useState,useEffect,useContext}  from "react";
+// import React,{useState,useEffect,useContext}  from "react";
+// import  ReactDOM  from "react-dom/client";
+// import reportWebVitals from "./reportWebVitals"; 
+
+// const employeeContext =React.createContext();
+// function App(){
+//   const[employee,setEmployee] = useState({Id:109,Name:'Saqib',Location:'karachi',Salary:4000})
+  
+//   return(<div>
+//     <h2>Welcome to App Component...</h2>
+//     <employeeContext.Provider value={employee}>
+//       <Employee></Employee>
+//     </employeeContext.Provider>
+//   </div>)
+//   }
+
+// function Employee(){
+// let context=useContext(employeeContext);
+// return(
+//   <div>
+//     <h2>Welcome to Employee Component ...</h2>
+//     <p>
+//       <label>Employee ID : <b>{context.Id}</b></label>
+//     </p>
+//     <p>
+//       <label>Employee Name : <b>{context.Name}</b></label>
+//     </p>
+//     <Salary></Salary>
+//   </div>
+// )
+// }
+
+// function Salary(){
+// let context=useContext(employeeContext);
+// return(<div>
+//   <h2>Welcome to Salary Component...</h2>
+//   <p>
+//     <label>Employee ID: <b>{context.Id}</b></label>
+//   </p>
+//   <p>
+//     <label>Employee Name: <b>{context.Salary}</b></label>
+//   </p>
+// </div>);
+// }
+
+// const element=<App></App>;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+// reportWebVitals();
+
+//#endregion
+
+
+//#region Exe-33 - UseContext Hook- Pass Data from Parent to Child and child to parent
+
+import React,{useState,useContext}  from "react";
 import  ReactDOM  from "react-dom/client";
 import reportWebVitals from "./reportWebVitals"; 
 
 const employeeContext =React.createContext();
 function App(){
-  const[employee,setEmployee] = useState({Id:109,Name:'Saqib',Location:'karachi',Salary:4000})
+  const[employee, setEmployee] = useState({Id:109, Name:'Saqib', Location:'karachi', Salary:4000, EmploymentType:'Contract'})
   
-  return(<div>
+  return(<div>  
     <h2>Welcome to App Component...</h2>
-    <employeeContext.Provider value={employee}>
+      <p>
+        <label>Employee Salary : <b>{employee.Salary}</b></label>
+      </p>
+    <employeeContext.Provider value={{data:employee,updateEmployee:setEmployee}}>
       <Employee></Employee>
     </employeeContext.Provider>
   </div>)
   }
 
 function Employee(){
+
 let context=useContext(employeeContext);
+
+function changeEmploymentType(){
+  context.updateEmployee({...context.data,EmploymentType:'Permanent'});
+}
+
 return(
   <div>
     <h2>Welcome to Employee Component ...</h2>
     <p>
-      <label>Employee ID : <b>{context.Id}</b></label>
+      <label>Employee ID : <b>{context.data.Id}</b></label>
     </p>
     <p>
-      <label>Employee Name : <b>{context.Name}</b></label>
+      <label>Employee Name : <b>{context.data.Name}</b></label>
     </p>
+    <employeeContext.Consumer>
+      {value => value.data.EmploymentType === 'Permanent'?<Permanent></Permanent>:<Contract></Contract>}
+    </employeeContext.Consumer>
+
+
+
+
+    <button onClick={changeEmploymentType}>Make Permanent</button>
     <Salary></Salary>
   </div>
 )
@@ -2224,15 +2296,37 @@ return(
 
 function Salary(){
 let context=useContext(employeeContext);
+
+function updateSalary(){
+context.updateEmployee({...context.data,Salary:15000});
+}
 return(<div>
   <h2>Welcome to Salary Component...</h2>
   <p>
-    <label>Employee ID: <b>{context.Id}</b></label>
+    <label>Employee ID: <b>{context.data.Id}</b></label>
   </p>
   <p>
-    <label>Employee Name: <b>{context.Salary}</b></label>
+    <label>Employee Name: <b>{context.data.Salary}</b></label>
   </p>
+  <button onClick={updateSalary}>Change Salary</button>
 </div>);
+}
+
+
+function Permanent(){
+  return (
+    <div>
+      <h2>Permanent Component Contents...</h2>
+    </div>
+  );
+}
+
+function Contract(){
+  return(
+    <div>
+      <h2>Contract Component Contents...</h2>
+    </div>
+  )
 }
 
 const element=<App></App>;
